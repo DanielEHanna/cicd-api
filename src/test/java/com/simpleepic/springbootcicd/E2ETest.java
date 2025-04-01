f188168c553d6fb7dfd9ab438a2c467f11e3c6a8
DanielEHanna <35130124+DanielEHanna@users.noreply.github.com>
2025-04-01 15:39:30 +0100

    Create E2ETest.java

---
+++ src/test/java/com/simpleepic/springbootcicd/E2ETest.java
@@ -0,0 +1,13 @@
+package com.simpleepic.springbootcicd.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class CarServiceE2ETest {
+
+    @Test
+    void sampleTest() {
+        System.out.println("E2E Test is running...");
+    }
+}

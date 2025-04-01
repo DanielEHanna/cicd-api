716a73d929ba78ca292fb0d9df7f26b3079e90f6
DanielEHanna <35130124+DanielEHanna@users.noreply.github.com>
2025-04-01 15:41:11 +0100

    Update E2ETest.java

--- src/test/java/com/simpleepic/springbootcicd/E2ETest.java
+++ src/test/java/com/simpleepic/springbootcicd/E2ETest.java
@@ -1,4 +1,4 @@
-package com.simpleepic.springbootcicd.service;
+package com.simpleepic.springbootcicd;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;

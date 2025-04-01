6a15c9e7d27fa03513f77568f0e1f3f7ab295ebd
DanielEHanna <35130124+DanielEHanna@users.noreply.github.com>
2025-04-01 15:48:25 +0100

    Update E2ETest.java

--- src/test/java/com/simpleepic/springbootcicd/E2ETest.java
+++ src/test/java/com/simpleepic/springbootcicd/E2ETest.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-public class CarServiceE2ETest {
+public class E2ETest {
 
     @Test
     void sampleTest() {
